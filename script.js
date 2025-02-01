f6e0fe15162161b714967d085044f21bd646a725
DerpyIsMe3 <73795841+Derpydumdum2012@users.noreply.github.com>
2025-02-01 17:04:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,26 @@ function playGame(playerChoice) {
     }
 
     document.getElementById('result').innerText = `You chose ${playerChoice}. Computer chose ${computerChoice}. ${result}`;
+    document.getElementById('retryButton').style.display = 'block';
+    disableGameButtons();
+}
+
+function disableGameButtons() {
+    const buttons = document.querySelectorAll('.buttons button');
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+function retryGame() {
+    document.getElementById('result').innerText = '';
+    document.getElementById('retryButton').style.display = 'none';
+    enableGameButtons();
+}
+
+function enableGameButtons() {
+    const buttons = document.querySelectorAll('.buttons button');
+    buttons.forEach(button => {
+        button.disabled = false;
+    });
 }
